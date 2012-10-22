409f2a39a8c6f216ddb7c2757a444036571c0fe0
guilhermemalfatti <guilhermemalfatti@192.168.37.134>
2012-10-22 18:42:36 -0200

    feito

--- html/index.php
+++ html/index.php
@@ -8,78 +8,80 @@
                 margin: 20px;
                 background-color: brown;
                 padding: 20px;
-                
+
             }
 
         </style>
         <script type="text/javascript">
-            var boxX= 20,//local conto seperior x
-                boxY = 30,//local conto seperior y
-                boxWidth = 350,//largura caixa
-                boxHeigth = 250,//altura caixa
-                ballRad = 10,//raio da bola
-                boxBoundX = boxHeigth +  boxX - ballRad,//limite à direita
-                boxBoundY = boxHeight + boxY - ballRad,//limite ao fundo
-                inBoxBoundX = boxX + ballRad,//limite a esquerda
-                inBoxBoundY =boxY + ballRad,//limite do topo
-                ballX = 50,//posição inicial x
-                ballY = 60,//posição inicial y
-                ctx,
-                ballDeslcX = 4,//deslocamento inicial x
-                ballDeslcY = 8;//deslocamento inicial y            
-        
-        function init(){
-            ctx = document.getElementById('can').getContext('2d');
-            ctx.lineWidth = ballRad;
-            ctx.fillStyle = "rgb(200, 0, 50)";
-            //moveBall();
-            setInterval(moveBall, 100);
-        }
-        
-        function moveBall(){
-            ctx.clearRect(boxX, boxY, boxWidth, boxHeigth);
-            moveEndCheck();
-ctx.beginPath();
-ctx.arc(ballX, ballY, ballRad, 0, Math.PI*2, true);
-ctx.fill();
-ctx.strokeRect(boxX, boxY, boxWidth, boxHeigth);
-        }
-        
-        function moveEndCheck(){
-            var nBallX = ballX + ballDeslcX, 
-                nBallY = ballY + ballDeslcY;
-                
-                if(nBallX > boxBoundX){
-                    ballDeslcX =-ballDeslcX;
-                    nBallX = boxBoundX;
-                }
-                if(nBallX < inBoxBoundX){
-                    nBallX = inBoxBoundX;
-                    ballDeslcX = -ballDeslcY;
-                }
-                if(nBallY > boxBoundY){
-                    nBallY = boxBoundY;
-                    ballDeslcY = -ballDeslcY;
-                }
-                if(nBallY < inBoxBoundY){
-                    nBallY = inBoxBoundY;
-                    ballDeslcY = -ballDeslcY;
-                }
-                
-                ballX = nBallX;
-                ballY = nBallY;
-                
-                function change(){
-                    ballDeslcX = Number(f.vh.value);
-                    ballDeslcY = Number(f.vv.value);
-                    return false;
-            }
-        }
-        
-            </script>
-        
+            var boxX = 20;//local conto seperior x
+            var boxY = 30;//local conto seperior y
+            var boxWidth = 350;//largura caixa
+            var boxHeigth = 250;//altura caixa
+            var ballRad = 10;//raio da bola
+            var boxBoundX = boxWidth +  boxX - ballRad;//limite à direita
+            var boxBoundY = boxHeigth + boxY - ballRad;//limite ao fundo
+                             
+            var inBoxBoundX = boxX + ballRad;//limite a esquerda
+            var inBoxBoundY =boxY + ballRad;//limite do topo
+            var ballX = 50;//posição inicial x
+            var ballY = 60;//posição inicial y
+            var ctx;
+            var ballDeslcX = 4;//deslocamento inicial x
+            var ballDeslcY = 8;//deslocamento inicial y            
+            
+                        function init(){
+                             
+                            ctx = document.getElementById('can').getContext('2d');
+                            ctx.lineWidth = ballRad;
+                            ctx.fillStyle = "rgb(200, 0, 50)";
+                            moveBall();
+                            setInterval(moveBall, 100);
+                        }
+                    
+                        function moveBall(){
+                            ctx.clearRect(boxX, boxY, boxWidth, boxHeigth);
+                            moveEndCheck();
+                            ctx.beginPath();
+                            
+                            ctx.arc(ballX, ballY, ballRad, 0, Math.PI*2, true);
+                            ctx.fill();
+                            ctx.strokeRect(boxX, boxY, boxWidth, boxHeigth);
+                        }
+                    
+                        function moveEndCheck(){
+                            var nBallX = ballX + ballDeslcX, 
+                                nBallY = ballY + ballDeslcY;     
+                                
+                               
+                            if(nBallX > boxBoundX){
+                                ballDeslcX =-ballDeslcX;
+                                nBallX = boxBoundX;
+                            }
+                            if(nBallX < inBoxBoundX){
+                                nBallX = inBoxBoundX;
+                                ballDeslcX = -ballDeslcY;
+                            }
+                            if(nBallY > boxBoundY){
+                                nBallY = boxBoundY;
+                                ballDeslcY = -ballDeslcY;
+                            }
+                            if(nBallY < inBoxBoundY){
+                                nBallY = inBoxBoundY;
+                                ballDeslcY = -ballDeslcY;
+                            }    
+                            
+                            ballX = nBallX;
+                            ballY = nBallY;
+                        }
+                     function change(){
+                                ballDeslcX = Number(f.vh.value);
+                                ballDeslcY = Number(f.vv.value);
+                                return false;
+                            }
+        </script>
+
     </head>
-    <body onload="init();">
+    <body onload="init()">
         <canvas id="can" width="400" height="300">Seu Navegador não suporta html 5</canvas>
         <br>
         <form name="f" id="f" onsubmit="return change();">
